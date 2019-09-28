ecdbf5a259cdd2e7efa2ddd3e74fdf44cb6c9c9a
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-28 16:04:38 -0400

    #update and #save pass all tests

--- lib/student.rb
+++ lib/student.rb
@@ -33,4 +33,31 @@ class Student
     DB[:conn].execute(sql)
   end
 
-end
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES ( ?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create
+    
+  end
+
+end # End of Class Student
